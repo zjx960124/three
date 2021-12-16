f528102f0c8438838760567456d5ffe3d9f1224a
zjx960124 <18965183850@163.com>
2021-12-16 23:13:33 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <canvas id="canvas" width="1000" height="1000"></canvas>
+    <div id="tooltip"></div>
+  </div>
+</template>
+
+<script>
+  import { Map } from "../utils/mapClass";
+  export default {
+    name: 'HelloWorld',
+    props: {
+    },
+    data() {
+      return {
+        data: null
+      };
+    },
+    mounted() {
+      new Map().init();
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #tooltip {
+    position: absolute;
+    z-index: 2;
+    background: white;
+    padding: 10px;
+    border-radius: 2px;
+    visibility: hidden;
+  }
+</style>
